6f59fa5c22b1cca9d14e8f9fd7ad0d730c005fff
Mo Beigi <me@mobeigi.com>
2024-12-03 16:54:46 +1100

    Add template file

--- src/Template.kt
+++ src/Template.kt
@@ -1,3 +1,6 @@
+/*
+ * Template file.
+ */
 fun main() {
     fun part1(input: List<String>): Int {
         return input.size
@@ -10,12 +13,12 @@ fun main() {
     // Test if implementation meets criteria from the description, like:
     check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
+    // Or read a large test input from the `src/DayXX_test.txt` file:
+    val testInput = readInput("DayXX_test")
     check(part1(testInput) == 1)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    // Read the input from the `src/DayXX.txt` file.
+    val input = readInput("DayXX")
     part1(input).println()
     part2(input).println()
 }
