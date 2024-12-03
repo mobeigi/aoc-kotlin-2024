9e095733f297f8574f7a52b9525418e2db561d7e
Mo Beigi <me@mobeigi.com>
2024-12-03 18:21:22 +1100

    Tidy up day 03 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -3,16 +3,21 @@
  */
 class Day03 {
     companion object {
-        // Constants here
+        const val NUMBER_MAX_LENGTH = 3
+        const val DO_COMMAND = "do()"
+        const val DONT_COMMAND = "don't()"
     }
-
+    
+    /**
+     * Use regexp to find each mul() function, and sum the inputs together.
+     */
     fun part1(input: List<String>): Int {
         return input.sumOf { line ->
             val components = line.split("mul(", ")")
             components.sumOf { component ->
                 val numbers = component.split(",")
-                if (numbers.size == 2 && numbers[0].isNotEmpty() && numbers[0].length <= 3 && numbers[0].all { it.isDigit() } &&
-                    numbers[1].isNotEmpty() && numbers[1].length <= 3 && numbers[1].all { it.isDigit() }) {
+                if (numbers.size == 2 && numbers[0].isNotEmpty() && numbers[0].length <= NUMBER_MAX_LENGTH && numbers[0].all { it.isDigit() } &&
+                    numbers[1].isNotEmpty() && numbers[1].length <= NUMBER_MAX_LENGTH && numbers[1].all { it.isDigit() }) {
                     numbers[0].toInt() * numbers[1].toInt()
                 } else {
                     0
@@ -21,6 +26,10 @@ class Day03 {
         }
     }
 
+    /**
+     * Preprocess input by disregarding all don't() sections in each line.
+     * Then delegate to part 1 solution.
+     */
     fun part2(input: List<String>): Int {
         var enabled = true
         val newInputs = input.map { line ->
@@ -28,26 +37,29 @@ class Day03 {
             var index = 0
             var newLine = ""
             line.forEach { _ ->
-                if (index + 4 <= line.length && line.substring(index, index + 4) == "do()") {
+                if (line.regionMatches(index, DO_COMMAND, 0, DO_COMMAND.length)) {
                     if (!enabled) {
                         enabled = true
                         startIndex = index
                     }
-                    index += 4
-                } else if (index + 7 <= line.length && line.substring(index, index + 7) == "don't()") {
+                    index += DO_COMMAND.length
+                } else if (line.regionMatches(index, DONT_COMMAND, 0, DONT_COMMAND.length)) {
                     if (enabled) {
                         enabled = false
                         newLine += line.substring(startIndex, index)
                     }
-                    index += 7
+                    index += DONT_COMMAND.length
                 }
                 index += 1
             }
             if (enabled) {
+                // Final append
                 newLine += line.substring(startIndex)
             }
             newLine
         }
+
+        // Delegate to part 1
         return part1(newInputs)
     }
 }
